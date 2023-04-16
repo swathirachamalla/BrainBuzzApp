64641a7675d325ae3208f024d3e8cd8c8f7ba7d3
swathirachamalla <55146310+swathirachamalla@users.noreply.github.com>
2023-04-16 11:06:52 +0530

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({path:'../.env'});
 
 const uri = process.env.MONGOURI;
-mongoose.connect(uri);
+mongoose.connect("mongodb+srv://rachamallaswathi3:aw3yYdh6R1Ifp9If@teacherquizapp.gumaaje.mongodb.net/?retryWrites=true&w=majority");
 
 mongoose.Promise = global.Promise;
 
